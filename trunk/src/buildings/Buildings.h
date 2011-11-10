ca2794297a1fb759ce1765358cddc3eb6c1a0abe
germanhc <germanhc@gmail.com>
2011-11-10 01:13:47 +0000

    agrego buildings

---
+++ trunk/src/buildings/Buildings.h
@@ -0,0 +1,40 @@
+#ifndef VM_H__
+#define VM_H__
+
+#include "ofImage.h"
+#include "ofxFenster.h"
+
+namespace mapinect {
+
+	class VM {
+	public:
+		virtual void setup(ofxFenster* f);
+		virtual void update();
+		virtual void draw();
+
+		virtual void keyPressed(int key);
+		virtual void mouseMoved(int x, int y );
+		virtual void mouseDragged(int x, int y, int button);
+		virtual void mousePressed(int x, int y, int button);
+		virtual void mouseReleased(int x, int y, int button);
+		virtual void windowResized(int w, int h);
+
+		ofImage img;
+		bool bImgLoaded;
+		int imgIndex;
+		char* imgFilename;
+		// ofTexture tex;
+
+		bool bTexApplied;
+		GLuint textureID;
+		GLuint streetT;
+
+		GLuint loadImageTexture(char* imgFile); 
+//		GLuint loadImageTexture(int& imgIndex); 
+
+		ofxFenster* fenster;
+
+	};
+}
+
+#endif	// VM_H__
