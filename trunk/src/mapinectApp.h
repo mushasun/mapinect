0b1cb558c1934d7e02cc7d756a24a56aad3ce7c8
rrivem@gmail.com <rrivem@gmail.com@96e75661-a97c-b5aa-2294-193d6a1a59eb>
2011-12-03 13:33:49 +0000

    - Some comments to mapinect framework - Color balancing testing application

--- trunk/src/mapinectApp.h
+++ trunk/src/mapinectApp.h
@@ -13,11 +13,20 @@
 #include "IApplication.h"
 
 namespace mapinect {
+
+	/// <summary>
+	/// Mapinect core. This class is responsible of initializating globals and each module.
+	/// It also drives the events to the modules.
+	/// User's application is treated as a yet another module.
+	/// </summary>
 	class mapinectApp : public ofBaseApp, public ofxFensterListener {
 	public:
 		mapinectApp(IApplication* app);
 		virtual ~mapinectApp();
 
+		/// <summary>
+		/// Events for the primary window (debug drawing area)
+		/// </summary>
 		virtual void setup();
 		virtual void update();
 		virtual void draw();
@@ -29,10 +38,14 @@ namespace mapinect {
 		virtual void mousePressed(int x, int y, int button);
 		virtual void mouseReleased(int x, int y, int button);
 		virtual void windowResized(int w, int h);
-
+		
+		/// <summary>
+		/// Events for the secondary window (user drawing area)
+		/// </summary>
 		virtual void fensterUpdate();
 		virtual void fensterSetup(); // Fenster setup added
 		virtual void fensterDraw();
+
 		virtual void fensterKeyPressed(int key);
 		virtual void fensterMouseMoved(int x, int y );
 		virtual void fensterMouseDragged(int x, int y, int button);
@@ -42,13 +55,20 @@ namespace mapinect {
 
 		int				angle;
 
+		/// <summary>
+		/// Framework modules
+		/// </summary>
 		OpenCV			cv;
 		PCM				pcm;
 		LPMT			lpmt;
 		VM				vm;
 		Arduino			arduino;
 
+		/// <summary>
+		/// User modules
+		/// </summary>
 		IApplication*	app;
+
 	};
 }
 
