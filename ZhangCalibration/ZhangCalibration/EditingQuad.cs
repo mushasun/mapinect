d3ac9b7e7799d9c49b073b01b58edc114a616076
rrivem <rrivem@gmail.com>
2012-01-13 10:30:08 +0000

    ZhangCalibration utility in C#

---
+++ ZhangCalibration/ZhangCalibration/EditingQuad.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace ZhangCalibration
+{
+	public class EditingQuad : NotifyPropertyChanged
+	{
+
+		public EditingQuad(Quad quad)
+		{
+			this.Quad = quad;
+			this.Polygon = new System.Windows.Shapes.Polygon();
+			Polygon.Stroke = System.Windows.Media.Brushes.Red;
+			Polygon.StrokeThickness = 1;
+			Binding binding = new Binding();
+			binding.Source = Quad;
+			binding.Path = new System.Windows.PropertyPath("Points");
+			Polygon.SetBinding(System.Windows.Shapes.Polygon.PointsProperty, binding);
+		}
+
+		~EditingQuad()
+		{
+		}
+
+		public System.Windows.Shapes.Polygon Polygon { get; private set; }
+
+		private Quad _Quad;
+		public Quad Quad
+		{
+			get
+			{
+				return _Quad;
+			}
+			set
+			{
+				_Quad = value;
+				FirePropertyChanged("Quad");
+			}
+		}
+
+		private int _EditingPointIndex;
+		public int EditingPointIndex
+		{
+			get
+			{
+				return _EditingPointIndex;
+			}
+			set
+			{
+				_EditingPointIndex = value;
+				FirePropertyChanged("EditingPointIndex");
+			}
+		}
+
+	}
+}
---
+++ ZhangCalibration/ZhangCalibration/NotifyPropertyChanged.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace ZhangCalibration
+{
+	public class NotifyPropertyChanged : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void FirePropertyChanged(string property)
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(property));
+			}
+		}
+
+	}
+}
