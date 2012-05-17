6dae89c3440164d3e1554e80dd63abbef1df2670
rrivem <rrivem@gmail.com>
2012-05-17 01:45:32 +0000

    back out de Features

---
+++ openFrameworks/apps/addonsExamples/mapinect/src/Feature.cpp
@@ -0,0 +1,59 @@
+#include "Feature.h"
+
+#include "ofxXmlSettings.h"
+
+namespace mapinect
+{
+	static bool gFeatures[FEATURE_COUNT];
+
+	#define		FEATURE_CONFIG			"FeatureConfig:FEATURE_"
+
+	void LoadFeatures()
+	{
+		for (int i = 0; i < FEATURE_COUNT; i++)
+		{
+			gFeatures[i] = true;
+		}
+
+		ofxXmlSettings XML;
+		if(XML.loadFile("Feature_Config.xml")) {
+			gFeatures[FEATURE_ARDUINO] = XML.getValue(FEATURE_CONFIG "ARDUINO", true);
+			gFeatures[FEATURE_KINECT] = XML.getValue(FEATURE_CONFIG "KINECT", true);
+			gFeatures[FEATURE_CV] = XML.getValue(FEATURE_CONFIG "CV", true);
+			gFeatures[FEATURE_PCM] = XML.getValue(FEATURE_CONFIG "PCM", true);
+			gFeatures[FEATURE_VM] = XML.getValue(FEATURE_CONFIG "VM", true);
+		}
+
+	}
+
+	bool IsFeatureActive(Feature feature)
+	{
+		return gFeatures[feature];
+	}
+
+	bool IsFeatureArduinoActive()
+	{
+		return IsFeatureActive(FEATURE_ARDUINO);
+	}
+
+	bool IsFeatureKinectActive()
+	{
+		return IsFeatureActive(FEATURE_KINECT);
+	}
+
+	bool IsFeatureCVActive()
+	{
+		return IsFeatureActive(FEATURE_CV);
+	}
+
+	bool IsFeaturePCMActive()
+	{
+		return IsFeatureActive(FEATURE_PCM);
+	}
+
+	bool IsFeatureVMActive()
+	{
+		return IsFeatureActive(FEATURE_VM);
+	}
+
+}
---
+++ openFrameworks/apps/addonsExamples/mapinect/src/Feature.h
@@ -0,0 +1,30 @@
+#ifndef FEATURE_H__
+#define FEATURE_H__
+
+namespace mapinect
+{
+	enum Feature
+	{
+		FEATURE_ARDUINO = 0,
+		FEATURE_KINECT,
+		FEATURE_CV,
+		FEATURE_PCM,
+		FEATURE_VM,
+
+		FEATURE_COUNT
+	};
+
+	void LoadFeatures();
+
+	bool IsFeatureActive(Feature feature);
+
+	bool IsFeatureArduinoActive();
+	bool IsFeatureKinectActive();
+	bool IsFeatureCVActive();
+	bool IsFeaturePCMActive();
+	bool IsFeatureVMActive();
+
+	#define CHECK_ACTIVE		if (!isActive()) return
+}
+
+#endif	// FEATURE_H__
