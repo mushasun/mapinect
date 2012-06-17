119afc410b6802b4511a7fbe7668cad020e564ed
rrivem <rrivem@gmail.com>
2012-06-17 01:19:44 +0000

    Todos los ejemplos compilando

--- openFrameworks/apps/addonsExamples/mapinect/apps/photoManager/src/photomanager/PhotoManager.h
+++ openFrameworks/apps/addonsExamples/mapinect/apps/photoManager/src/photomanager/PhotoManager.h
@@ -11,18 +11,27 @@ namespace photo {
 	class PhotoManager : public IApplication {
 	public:
 		virtual void setup();
-		virtual void exit();
 		virtual void update();
 		virtual void draw();
+		virtual void exit();
 
 		virtual void keyPressed(int key);
-		virtual void mouseMoved(int x, int y );
+		virtual void keyReleased(int key);
+		virtual void windowMoved(int x, int y);
+		virtual void mouseMoved(int x, int y);
 		virtual void mouseDragged(int x, int y, int button);
 		virtual void mousePressed(int x, int y, int button);
 		virtual void mouseReleased(int x, int y, int button);
-		virtual void windowResized(int w, int h);
-	
+		virtual void dragEvent(ofDragInfo info);
+
 		virtual void debugDraw();
+
+		virtual void objectDetected(const IObjectPtr&);
+		virtual void objectUpdated(const IObjectPtr&);
+		virtual void objectLost(const IObjectPtr&);
+		virtual void objectMoved(const IObjectPtr&, const DataMovement&);
+		virtual void objectTouched(const IObjectPtr&, const DataTouch&);
+
 		static GLuint	tableTexture;
 	private:
 		bool	isHandInPhoto(PCHand* hand,Photo* photo);
