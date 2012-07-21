36bd1e2187771768a42e90ecad1986cb3cd764ec
rrivem@gmail.com <rrivem@gmail.com@96e75661-a97c-b5aa-2294-193d6a1a59eb>
2012-07-21 00:18:38 +0000

    [Vero] Arregle bug en Arduino, que se generó cuando empecé a usar gTransformation, estaba obteniendo mal la transformacion y por eso no se veía nada en la ventana del mapping

--- openFrameworks/apps/addonsExamples/mapinect/src/arduino/Transformation.cpp
+++ openFrameworks/apps/addonsExamples/mapinect/src/arduino/Transformation.cpp
@@ -3,6 +3,7 @@
 namespace mapinect
 {
 	Eigen::Affine3f Transformation::worldTransformation;
+	Eigen::Affine3f Transformation::inverseWorldTransformation;
 	Eigen::Affine3f Transformation::initialWorldTransformation;
 
 	Transformation::Transformation() 
@@ -21,11 +22,23 @@ namespace mapinect
 		}
 	}
 
+	Eigen::Affine3f	Transformation::getInverseWorldTransformation() const
+	{
+		{
+			transformationMatrixMutex.lock();
+			Eigen::Affine3f transf (inverseWorldTransformation);
+			transformationMatrixMutex.unlock();
+			return transf;
+		}
+	}
+
+
 	void Transformation::setWorldTransformation(const Eigen::Affine3f& newTransformation)
 	{
 		{
 			transformationMatrixMutex.lock();
 			worldTransformation = newTransformation;
+			inverseWorldTransformation = newTransformation.inverse();
 			transformationMatrixMutex.unlock();
 		}
 	}
